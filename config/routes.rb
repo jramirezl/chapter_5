4e12332e2c172ddf08017d8408924f54cbed4329
Juan Ramirez <jpramirezl@gmail.com>
2014-02-24 15:11:08 -0600

    Finish chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Chapter3::Application.routes.draw do
+  root to: 'static_pages#home'
+
   match '/signup',  to: 'users#new'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/home' => 'static_pages#home'
+  match '/' => 'static_pages#home'
 
-  root to: 'static_pages#home'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
